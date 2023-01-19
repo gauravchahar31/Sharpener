b6d28fd720aa9f1698976019bef60b9c0d94d2d1
Gaurav Kumar <76642994+gauravchahar31@users.noreply.github.com>
2023-01-19 18:27:58 +0530

    REST Project

---
+++ REST API PRoject/script.js
@@ -0,0 +1,69 @@
+let arrayOfBills = [];
+
+const axiosInstance = axios.create({
+    baseURL: 'https://crudcrud.com/api/147f818cc6ed4b5381d906d38838ec1f'
+})
+
+window.addEventListener('DOMContentLoaded', (event) => {
+   fetchFromServer();
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetchFormData();
+})
+
+function fetchFormData(){
+    const billAmount = document.getElementById('bill').value;
+    const dishName = document.getElementById('dishname').value;
+    const tableNo = document.getElementById('tableno').value;
+    const tableData = {
+        billAmount : billAmount, 
+        dishName : dishName, 
+        tableNo : tableNo
+    };
+    saveToServer(tableData);
+}
+
+function addBillToList(tableData){
+    const container = document.querySelector(`#table${tableData.tableNo}`);
+    const newLi = document.createElement('li');
+    newLi.setAttribute("class", "list-group-item");
+    newLi.innerHTML = `${tableData.billAmount} - ${tableData.dishName}`;
+    container.appendChild(newLi);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = "X";
+    deleteButton.setAttribute("class", "btn btn-danger btn-sm float-right delete")
+    newLi.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        deleteFromServer(tableData);
+        container.removeChild(newLi);
+    })
+}
+
+function fetchFromServer(){
+    axiosInstance.get('/bills/')
+    .then(res => {
+        arrayOfBills = res.data;
+        arrayOfBills.forEach(bills => {
+            addBillToList(bills);
+        })
+    })
+    .catch(err => console.error(err));
+}
+
+function saveToServer(tableData){
+    axiosInstance.post('/bills/', tableData)
+        .then( res => {addBillToList(tableData)})
+        .catch(err => console.error(err));
+}
+
+function deleteFromServer(tableData){
+    axiosInstance.delete(`/bills/${tableData._id}`)
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+}
